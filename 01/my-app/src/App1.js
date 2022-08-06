5937d94019beaa9b3734923ff207899bcb5d5c30
changxianglin <281532612@qq.com>
2022-08-06 18:35:54 +0800

    react uesEffect

---
+++ 01/my-app/src/App1.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './App.css';
+
+// function App() {
+//   const [count, setCount] = useState(0)
+//   return (
+//     <div className="App">
+//       <p>You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>
+//         Click me
+//       </button>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <p>You clicked {this.state.count} times</p>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Click me
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
